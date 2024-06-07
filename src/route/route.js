07f6473aef07fc653bb632e414fc1efd0c5e316b
madeputrateg <baruputra111@gmail.com>
2024-06-07 11:41:36 +0700

    changed data

--- src/route/route.js
+++ src/route/route.js
@@ -1,14 +1,14 @@
 const express = require("express")
-const verify = require("./middleware/auth")
+const verifymiddleware = require("./middleware/auth")
 
-const intilizeroute = (express)=>{
+const intilizeroute = (expressNode)=>{
     const newmiddleware = express()
     newmiddleware.use("/middleware",verifymiddleware)
     newmiddleware.get("/middleware/test",(req,res)=>{
     console.log(req.datapayload)
     res.send("passed")
 })
-    express.use(newmiddleware)
+    expressNode.use(newmiddleware)
 }
 
 module.exports = getprofileroute
